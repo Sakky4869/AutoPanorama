21f5d76c6688f058752374f82e1d35af3ad7f7eb
Sakky4869 <jcjmwh89.private@gmail.com>
2022-10-01 15:05:23 +0900

    view_common.phpを作成 #13 アプリの設定クラスおよび，セッション管理クラスのインスタンスを，当ファイルで初期化

---
+++ view_common.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once(dir(__FILE__) . '/session_manager.php');
+require_once(dir(__FILE__) . '/app_config.php');
+
+// アプリの設定クラスを初期化
+$app_config = new AppConfig();
+
+// セッション管理クラスを初期化
+$session_manager = new SessionManager();
+
+?>
