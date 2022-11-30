610945a43cbf97accb2d220bd82d7fbaada8ca29
Sakky4869 <jcjmwh89.private@gmail.com>
2022-11-30 15:38:35 +0900

    #46 Webアプリのブランチを整理 Webアプリ関係以外のファイルを削除． 今後，developブランチにはマージしない．

--- clear_match_datas.sh
+++
@@ -1,3 +0,0 @@
-# read -p "detect_resultおよび，detect_progressのデータを全消去しますか？(y/N): " yn
-# case "$yn" in [yY]*) ;; *) echo "abort."; exit;;esac
-
--- image_process_dev/config.hpp
+++
@@ -1,4 +0,0 @@
-// OpenCVのMatの書き出しモード
-// 1：imshowで表示
-// 0：output_imgディレクトリに書き込み
-#define SHOW_MODE 0
